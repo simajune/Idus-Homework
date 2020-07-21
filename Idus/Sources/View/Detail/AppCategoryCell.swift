aff5e7e1cd77725f962209ff22a24a9d9fdb3fa5
정태준 <tejayjung@gmail.com>
2020-07-21 17:27:18 +0900

    Add 디테일 뷰컨트롤러에 필요한 파일 생성

---
+++ Idus/Sources/View/Detail/AppCategoryCell.swift
@@ -0,0 +1,9 @@
+//
+//  AppCategoryCell.swift
+//  Idus
+//
+//  Created by Taejune Jung on 2020/07/21.
+//  Copyright © 2020 TJ. All rights reserved.
+//
+
+import Foundation
---
+++ Idus/Sources/View/Detail/AppInfoCell.swift
@@ -0,0 +1,9 @@
+//
+//  AppInfoCell.swift
+//  Idus
+//
+//  Created by Taejune Jung on 2020/07/21.
+//  Copyright © 2020 TJ. All rights reserved.
+//
+
+import Foundation
